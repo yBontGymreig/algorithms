fe886a7913fb48bdc1ef840adec450d93f3f288c
YBontGymreig <davee@shrewsbury.ac.uk>
2022-06-15 08:51:41 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int [] items = {4,54,5,7,4};
+            Console.WriteLine(LinearSearch(items, 4));
+        }
+        /*
+        * Returns index of item, or -1 if item is not found.
+        */
+        static int LinearSearch(int [] items, int target){
+            int currentIndex = 0;
+            int end = items.Length-1;
+
+            while (end < currentIndex){
+                if(items[currentIndex]==target){
+                    return currentIndex;
+                }
+                else{
+                    currentIndex++;
+                }
+            }
+            return -1;
+        }
+    }
+}
