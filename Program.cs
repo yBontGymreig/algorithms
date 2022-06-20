8ab245f3167ff3cfc403a3fadbf10c1e84aefa59
Dave Edwards <edwardsd@hlcollege.ac.uk>
2022-06-20 17:02:41 +0100

    after gitautoconfig

--- Program.cs
+++ Program.cs
@@ -6,25 +6,93 @@ namespace algorithms
     {
         static void Main(string[] args)
         {
-            int [] items = {4,54,5,7,4};
-            Console.WriteLine(LinearSearch(items, 4));
+            int[] items = { 44, 54, 5, 7, 4 };
+            BadBubbleSort(items);
+            //PrintArrayOfInts(items);
         }
+
+
         /*
         * Returns index of item, or -1 if item is not found.
         */
-        static int LinearSearch(int [] items, int target){
+        static int LinearSearch(int[] items, int target)
+        {
             int currentIndex = 0;
-            int end = items.Length-1;
+            int end = items.Length - 1;
 
-            while (end < currentIndex){
-                if(items[currentIndex]==target){
+            while (end < currentIndex)
+            {
+                if (items[currentIndex] == target)
+                {
                     return currentIndex;
                 }
-                else{
+                else
+                {
                     currentIndex++;
                 }
             }
             return -1;
+        }
+
+        static void BubbleSort(int[] items)
+        {
+            bool swapped = true;
+            int end = items.Length - 2;
+            while (swapped)
+            {
+                //check whether a swap has been made on a pass. No swap means items are sorted
+                swapped = false;
+                // on each pass, iterate through items, stopping one position earlier on each pass
+                for (int i = 0; i <= end; i++)
+                {
+                    //if pair of neighbouring items are out of order
+                    if (items[i] > items[i + 1])
+                    {
+                        //swap
+                        int temp = items[i + 1];
+                        items[i + 1] = items[i];
+                        items[i] = temp;
+                        swapped = true;
+                    }
+                }
+                //after a pass, top item is sorted so you can end one pos earlier
+                end--;
+
+                PrintArrayOfInts(items);
+
+            }
+
+        }
+
+        static void BadBubbleSort(int[] items)
+        {
+            for (int pass = 1; pass <= items.Length-1; pass++)
+            {
+                // on each pass, iterate through items, stopping one position earlier on each pass
+                for (int i = 0;  i < items.Length-1; i++)
+                {
+                    //if pair of neighbouring items are out of order
+                    if (items[i] > items[i + 1])
+                    {
+                        //swap
+                        int temp = items[i + 1];
+                        items[i + 1] = items[i];
+                        items[i] = temp;
+                    }
+                }
+                PrintArrayOfInts(items);
+            }
+        }
+
+
+        static void PrintArrayOfInts(int[] nums)
+        {
+            //TODO it would be nicer to print these across the screen rather than one on each line.
+            foreach(int num in nums)
+            {
+                Console.Write(num + " ");
+            }
+            System.Console.WriteLine("");
         }
     }
 }
